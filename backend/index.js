88ce21d4e506aae86429cb4922be6105007c80c3
Pritam <pritamdhaugoda@gmail.com>
2023-09-08 14:17:28 +0545

    Added routes to get data from database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
-// Import necessary libraries/modules
 import express from "express"; // Import the Express framework
 import mongoose from "mongoose"; // Import Mongoose for MongoDB connectivity
 import AdminJSExpress from "@adminjs/express"; // Import AdminJS Express module
-import adminJs from './Admin.js'; // Import your AdminJS configuration
-import { fetchAndSavePlatforms } from './Api/Add/Platforms.js';
-import { fetchAndSaveGenres } from './Api/Add/Genre.js';
-import { fetchAndSavePublishers } from "./Api/Add/Publisher.js";
-import { fetchAndSaveStores } from "./Api/Add/Store.js";
-import { fetchAndSaveGame } from "./Api/Add/Game.js";
+import adminJs from "./Admin.js"; // Import your AdminJS configuration
+import gamesRoutes from "./Routes/Game.js";
+import platformRoutes from "./Routes/Platform.js";
+import publisherRoutes from "./Routes/Publisher.js";
+import genreRoutes from "./Routes/Genre.js";
+import storesRoute from "./Routes/Store.js";
 
 // Create an Express application instance
 const app = express();
@@ -16,70 +15,22 @@ const app = express();
 const router = AdminJSExpress.buildRouter(adminJs); // Create an AdminJS router using your configuration
 app.use(adminJs.options.rootPath, router); // Use the AdminJS router with the specified root path
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.post("/add-platforms",async(req,res)=>{
-    try {
-        // Call the fetchAndSavePlatforms function to fetch and save platforms
-        await fetchAndSavePlatforms();
-        res.send("Platforms added successfully");
-    } catch (error) {
-        console.error('Error adding platforms:', error.message);
-        res.send("Error adding platforms");
-    }
-})
-
-app.post("/add-genres",async(req,res)=>{
-    try {
-        // Call the fetchAndSaveGenres function to fetch and save genres
-        await fetchAndSaveGenres();
-        res.send("Genres added successfully");
-    } catch (error) {
-        console.error('Error adding genres:', error.message);
-        res.send("Error adding genres");
-    }
-})
-
-app.post("/add-publishers",async(req,res)=>{
-    try {
-        await fetchAndSavePublishers();
-        res.send("Publishers added successfully");
-    } catch (error) {
-        console.error('Error adding Publishers:', error.message);
-        res.send("Error adding Publishers");
-    }
-})
-
-app.post("/add-stores",async(req,res)=>{
-    try {
-        await fetchAndSaveStores();
-        res.send("Stores added successfully");
-    } catch (error) {
-        console.error('Error adding Stores:', error.message);
-        res.send("Error adding Stores");
-    }
-})
-
-app.post("/add-game/:id",async(req,res)=>{
-    const id = req.params.id;
-    try {
-        await fetchAndSaveGame({id});
-        res.send("Game added successfully");
-    } catch (error) {
-        console.error('Error adding Game:', error.message);
-        res.send("Error adding Game");
-    }
-})
+// Use routes for the games
+app.use("/", gamesRoutes,platformRoutes,genreRoutes,publisherRoutes,storesRoute);
 
 // Run the server.
 const run = async () => {
-    // Connect to MongoDB
-    await mongoose.connect("mongodb://mongodb:27017/GamesDB");
-    
-    // Start the Express server on port 5000 and log a message once it's listening
-    await app.listen(5000, () => console.log(`Example app listening on port 5000`));
+  // Connect to MongoDB
+  await mongoose.connect("mongodb://mongodb:27017/GamesDB");
+
+  // Start the Express server on port 5000 and log a message once it's listening
+  await app.listen(5000, () =>
+    console.log(`Example app listening on port 5000`)
+  );
 };
 
 run(); // Call the run function to start the server
